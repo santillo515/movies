61625e53330d45b0ef418ef78dda752edba8b033
santillo515 <59385575+santillo515@users.noreply.github.com>
2020-10-13 00:59:13 +0700

    metrika init

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -63,5 +63,19 @@
     <script src="https://use.fontawesome.com/b79841f35d.js" defer></script>
     <livewire:scripts></livewire:scripts>
     @yield('scripts')
+    <!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+    ym(56427112, "init", {
+         clickmap:true,
+         trackLinks:true,
+         accurateTrackBounce:true
+    });
+ </script>
+ <noscript><div><img src="https://mc.yandex.ru/watch/56427112" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+ <!-- /Yandex.Metrika counter -->
 </body>
 </html>
